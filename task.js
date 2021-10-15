2fad92d4fcadee27e327e3f5f3e2ec7c2baae9d1
dcbastien <ddcbastien@gmail.com>
2021-10-14 20:05:26 -0500

    Created index.html boilerplate and intro alert for Task Manager in task.js

---
+++ task.js
@@ -0,0 +1,20 @@
+//For selecting task
+let option= true;
+
+//If close is selected
+
+
+
+
+prompt(
+`TASK MANAGER
+
+What would you like to do (Please enter one of the options below):
+"TASKS" - Displays All Tasks
+"NEW" - Add a New Task
+"REMOVE" - Remove a Task
+"CLOSE" - Close The Task Manager
+        
+        `)
+//If close is the users selection
+alert(`Thank you for using Task Manager`)
